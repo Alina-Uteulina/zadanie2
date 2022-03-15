0c6e7b12fa69efca01476d598bf5ea6a6fe3f41c
Alina-Uteulina <a.uteulina@mail.ru>
2022-03-15 15:45:25 +0300

    Update данные.py

--- данные.py
+++ данные.py
@@ -1,64 +1,111 @@
 import numpy as np
 import math as mh
-# Пузырьковый режим
-p_l = int(input())
-lambda_l = int(input())
-p_g = int(input())
-m_l = int(input())
-m_g = int(input())
-g: float = 9.8
-theta: int = 90
-f_tr = int(input())
-v_s = int(input())
-d = int(input())
-q_l = int(input())
-a_p = int(input())
-# параметры двухфазного потока
-p_tr = p_l * lambda_l + p_g * (1 - lambda_l)
-m_tr = m_l * lambda_l + m_g * (1 - lambda_l)
-v_sl = q_l / a_p
-v_sg = 0.25 * v_s + 0.333 * v_sl
-v_tr = v_sl + v_sg
 
 
-# Пробковый режим
-beta = int(input())
-H_LLS = int(input())
-f_ls = int(input())
-m_ls = int(input())
-v_gtb = int(input())
-v_gls = int(input())
-v_ltb = int(input())
-H_LTB = int(input())
-v_lls = int(input())
-# параметры
-p_ls = p_l * H_LLS + p_g * (1 - H_LLS)
-v_sg1 = beta * v_gtb * (1 - H_LTB) + (1 - beta) * v_gls * (1 - H_LLS)
-v_ls1 = (1 - beta) * v_lls * H_LLS - beta * v_ltb * H_LTB
-v_m = v_sg1 + v_ls1
+class Parametrs:
 
+    def __init__(self, d, p_l, lambda_l, p_g, m_l, m_g, g_l, a_p, v_s, beta, h_lls, f_ls, m_ls, v_gtb, v_gls, v_ltb,
+                 h_ltb, v_lls, c_0, theta, p_c, sigma_l, f_sc, g_g, delta, g):
+        self.d = d
+        self.p_l = p_l
+        self.lambda_l = lambda_l
+        self.p_g = p_g
+        self.m_g = m_g
+        self.m_l = m_l
+        self.g_l = g_l
+        self.a_p = a_p
+        self.v_s = v_s
+        self.beta = beta
+        self.h_lls = h_lls
+        self.f_ls = f_ls
+        self.m_ls = m_ls
+        self.v_gtb = v_gtb
+        self.v_gls = v_gls
+        self.v_ltb = v_ltb
+        self.h_ltb = h_ltb
+        self.v_lls = v_lls
+        self.c_0 = c_0
+        self.theta = theta
+        self.p_c = p_c
+        self.sigma_l = sigma_l
+        self.f_sc = f_sc
+        self.g_g = g_g
+        self.delta = delta
+        self.g = g
 
-# Эмульсионный режим
-C_0 = int(input())
-# параметры двухфазного потока
-v__sg = np.sin(theta) / (4 - C_0) * C_0 * v_sl + v_s
+    def pp_puz(self):
+        p_tr = self.p_l * self.lambda_l + self.p_g * (1 - self.lambda_l)
+        return p_tr
 
+    def mp_puz(self):
+        m_tr = self.m_l * self.lambda_l + self.m_g * (1 - self.lambda_l)
+        return m_tr
 
-# Кольцевой режим
-p_c = int(input())
-sigma_l = int(input())
-f_sc = int(input())
-q_g = int(input())
-delta: float = 0.1
-# параметры двухфазного потока
-v_sg3 = q_g / a_p
-v_kr = 10000 * v_sg3 * m_g / sigma_l * (p_g / p_l) ** 0.5
-f_e = 1 - mh.exp((-0.125)*(v_kr - 1.5))
-v_sc = f_e * v_sl + v_sg3
-dp = f_sc * p_c * v_sc ** 2 / 2 * d
-if f_e > 0.9:
-    z = 1 + 300 * delta
-else:
-    z = 1 + 24 * delta * (p_l / p_g) ** (1 / 3)
-dp_c = z / (1 - 2 * delta) ** 5 * dp + p_c * g * np.sin(theta)
-fi = dp_c - p_c * g * np.sin(theta) / dp
+    def v_puz(self):
+        v_sl = self.g_l / self.a_p
+        return v_sl
+
+    def vs_puz(self, v_sl):
+        v_sg = 0.25 * self.v_s + 0.333 * v_sl
+        return v_sg
+
+    def vt_puz(self, v_sl, v_sg):
+        v_tr = v_sl + v_sg
+        return v_tr
+    """Пробковый режим"""
+    def p_pr(self):
+        p_ls = self.p_l * self.h_lls + self.p_g * (1 - self.h_lls)
+        return p_ls
+
+    def v_pr(self):
+        v_sg1 = self.beta * self.v_gtb * (1 - self.h_ltb) + (1 - self.beta) * self.v_gls * (1 - self.h_ltb)
+        return v_sg1
+
+    def vl_pr(self):
+        v_ls1 = (1 - self.beta) * self.v_lls * self.h_lls - self.beta * self.v_ltb * self.h_ltb
+        return v_ls1
+
+    def vm_pr(self, v_sg1, v_ls1):
+        v_m = v_sg1 + v_ls1
+        return v_m
+
+    """Эмульсионный режим"""
+    def v_mus(self, v_sl):
+        v__sg = np.sin(self.theta) / (4 - self.c_0) * self.c_0 * v_sl + self.v_s
+        return v__sg
+
+    """Кольцевой режим"""
+    def v_kol(self):
+        v_sg3 = self.g_g / self.a_p
+        return v_sg3
+
+    def vk_kol(self, v_sg3):
+        v_kr = 10000 * v_sg3 * self.m_g / self.sigma_l * (self.p_g / self.p_l) ** 0.5
+        return v_kr
+
+    def f_kol(self, v_kr):
+        f_e = 1 - mh.exp((-0.125)*(v_kr - 1.5))
+        return f_e
+
+    def vs_kol(self, f_e, v_sl, v_sg3):
+        v_sc = f_e * v_sl + v_sg3
+        return v_sc
+
+    def df_kol(self, v_sc):
+        dp = self.f_sc * self.p_c * v_sc ** 2 / 2 * self.d
+        return dp
+
+    def z_kol(self, f_e):
+        if f_e > 0.9:
+            z = 1 + 300 * self.delta
+        else:
+            z = 1 + 24 * self.delta * (self.p_l / self.p_g) ** (1 / 3)
+        return z
+
+    def dp_c_kol(self, z, dp):
+        dp_c = z / (1 - 2 * self.delta) ** 5 * dp + self.p_c * self.g * np.sin(self.theta)
+        return dp_c
+
+    def fi_kol(self, dp_c, dp):
+        fi = dp_c - self.p_c * self.g * np.sin(self.theta) / dp
+        return fi
