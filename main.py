79cd13830d67b31a7c3a93f60957e98b23b5e402
Alina-Uteulina <a.uteulina@mail.ru>
2022-03-16 13:23:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,21 +9,31 @@ theta: int = 90
 
 
 class Ansari:
-    def param(self, d, p_l, lambda_l, p_g, m_g, m_l, g_l, a_p, v_s, beta, h_lls, f_ls, m_ls, v_gtb, v_gls, v_ltb, h_ltb,
-              v_lls, c_0, theta, p_c, sigma_l, f_sc, g_g, delta, g):
-        p = Parametrs(d, p_l, lambda_l, p_g, m_g, m_l, g_l, a_p, v_s, beta, h_lls, f_ls, m_ls, v_gtb, v_gls, v_ltb,
-                      h_ltb, v_lls, c_0, theta, p_c, sigma_l, f_sc, g_g, delta, g)
-        return p
-
-    def __init__(self, d, theta, p_tr, f_tr, p_ls, f_ls):
+    def __init__(self, d, theta, p_tr, f_tr, p_ls, f_ls, p_c, p_l, p_g, sigma_l, beta, v_s):
         self.d = d
         self.theta = theta
         self.p_tr = p_tr
         self.f_tr = f_tr
         self.p_ls = p_ls
         self.f_ls = f_ls
+        self.p_c = p_c
+        self.p_l = p_l
+        self.p_g = p_g
+        self.sigma_l = sigma_l
+        self.beta = beta
+        self.v_s = v_s
 
-    def calc_params(self, g_l, a_p, g_g, v_sl, v_sg, v_m):
+    def dan(self, d, p_l, lambda_l, p_g, m_g, m_l, g_l, a_p, v_s, beta, h_lls, f_ls, m_ls, v_gtb, v_gls, v_ltb, h_ltb,
+            v_lls, c_0, theta, p_c, sigma_l, f_sc, g_g, delta):
+        p = Parametrs(d, p_l, lambda_l, p_g, m_g, m_l, g_l, a_p, v_s, beta, h_lls, f_ls, m_ls, v_gtb, v_gls, v_ltb,
+                      h_ltb, v_lls, c_0, theta, p_c, sigma_l, f_sc, g_g, delta)
+        return p
+
+    def calc_params(self, g_l, a_p, g_g, v_sl, v_sg, v_m, lambda_l, m_g, m_l, h_lls, m_ls, v_gtb, v_gls, v_ltb, h_ltb,
+                    v_lls, c_0, theta, p_c, sigma_l, f_sc, delta, d):
+        self.p = self.dan(d, p_l, lambda_l, p_g, m_g, m_l, g_l, a_p, v_s, beta, h_lls, f_ls, m_ls, v_gtb, v_gls, v_ltb,
+                          h_ltb, v_lls, c_0, theta, p_c, sigma_l, f_sc, g_g, delta)
+
         self.v_sl = g_l/a_p
 
         self.v_sg = g_g/a_p
@@ -136,42 +146,51 @@ class Ansari:
             grad_mus = funct_gmus + funct_mus
         return grad_mus
 
-    def kol(self, fp, fi, dp, p_c, theta):
+    def kol(self, fp, p_c, theta, p):
         """
         расчет давления для кольцевого режима
 
         Parameters
         ----------
         :param fp: номер режима потока
-        :param fi: коэффициент
         :param theta: угол наклона трубы
-        :param dp: состовляющая градиента давления по трению для газового ядра
         :param p_c: плотность газового ядра
         """
+
+        v_sg3 = p.v_kol()
+        v_kr = p.vk_kol(v_sg3)
+        f_e = p.f_kol(v_kr)
+        v_sc = p.vs_kol(f_e, self.v_sl, v_sg3)
+        dp = p.df_kol(v_sc)
+        z = p.z_kol(f_e)
+        dp_c = p.dp_c_kol(z, dp)
+        fi = p.fi_kol(dp_c, dp)
+
         if fp == 4:
             funct_gkol = fi * dp  # гравитационная составляющая
             funct_tkol = 9.81 * p_c * np.sin(theta)  # составляющая по трению
             grad_kol = funct_gkol + funct_tkol
         return grad_kol
 
-    def grad(self, g_l, g_g, a_p, p_g, v_s, sigma_l, p_l, beta, p_ls, f_ls, fi, dp, p_c, p):
-        self.calc_params(g_l, a_p, g_g, self.v_sl, self.v_sg, self.v_m)
+    def grad(self, g_l, g_g, a_p, lambda_l, m_g, m_l, h_lls, m_ls, v_gtb, v_gls, v_ltb, h_ltb, v_lls, c_0, f_sc, delta):
+        self.calc_params(g_l, a_p, g_g, self.v_sl, self.v_sg, self.v_m, lambda_l, m_g, m_l, h_lls, m_ls, v_gtb, v_gls,
+                         v_ltb, h_ltb, v_lls, c_0, theta, p_c, sigma_l, f_sc, delta, d)
 
-        fp = self.calc_fp(self.v_sl, v_s, sigma_l, p_l, p_g, p)
+        fp = self.calc_fp(self.v_sl, self.v_s, self.sigma_l, self.p_l, self.p_g, self.p)
         if fp == 1:
-            gr = self.puz(fp, self.p_tr, theta, self.f_tr, self.v_tr, self.d)
+            gr = self.puz(fp, self.p_tr, self.theta, self.f_tr, self.v_tr, self.d)
         if fp == 2:
-            gr = self.prob(fp, beta, p_ls, p_g, theta, f_ls, self.v_m, self.d)
+            gr = self.prob(fp, self.beta, self.p_ls, self.p_g, self.theta, self.f_ls, self.v_m, self.d)
         if fp == 3:
-            gr = self.mus(fp, self.p_tr, theta, self.f_tr, self.v_tr, self.d)
+            gr = self.mus(fp, self.p_tr, self.theta, self.f_tr, self.v_tr, self.d)
         if fp == 4:
-            gr = self.kol(fp, fi, dp, p_c, theta)
+            gr = self.kol(fp, self.p_c, self .theta, self.p)
         return gr
 
 
-def gradient(d, theta, p_tr, f_tr, p_ls, f_ls, g_l, a_p, g_g, p_g, v_s, sigma_l, p_l, beta, fi, dp, p_c, p):
-    ans = Ansari(d, theta, p_tr, f_tr, p_ls, f_ls)
-    dp = ans.grad(g_l, a_p, g_g, p_g, v_s, sigma_l, p_l, beta, p_ls, f_ls, fi, dp, p_c, p)
+def gradient(g_l, a_p, g_g, lambda_l, m_g, m_l, h_lls, m_ls, v_gtb, v_gls, v_ltb, h_ltb, v_lls, c_0, f_sc, delta):
+    ans = Ansari(d, theta, p_tr, f_tr, p_ls, f_ls, p_c, p_l, p_g, sigma_l, beta, v_s)
+    dp = ans.grad(g_l, a_p, g_g, lambda_l, m_g, m_l, h_lls, m_ls, v_gtb, v_gls, v_ltb, h_ltb, v_lls, c_0, f_sc, delta)
     return dp
 
 
@@ -179,9 +198,36 @@ g_g = 100
 g_l = 150
 d = 60
 t = 30
-
+a_p = 20
+p_g = 100
+v_s = 50
+sigma_l = 50
+p_l = 100
+beta = 10
+fi = 100
+p_c = 10
+p = 1
+p_tr = 1
+f_tr = 2
+p_ls = 1
+f_ls = 2
+lambda_l = 1
+m_g = 1
+m_l = 1
+h_lls = 1
+m_ls = 1
+v_gtb = 1
+v_gls = 1
+v_ltb = 1
+h_ltb = 1
+v_lls = 1
+c_0 = 1
+f_sc = 1
+delta = 1
+g = 9.8
 
 result = solve_ivp(gradient, t_span=[0, 2000],
-                   y0=np.array([150]), args=(g_g, g_l, d, theta, t))
+                   y0=np.array([150]), args=(g_l, a_p, g_g, lambda_l, m_g, m_l, h_lls, m_ls, v_gtb, v_gls, v_ltb,
+                                             h_ltb, v_lls, c_0, f_sc, delta, t))
 plt.plot(result.y0)
 print(result)
