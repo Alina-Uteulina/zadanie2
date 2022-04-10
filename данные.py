752bfe6b62261edfd712aa05ff9b51c756f71417
Alina-Uteulina <a.uteulina@mail.ru>
2022-04-11 02:06:30 +0300

    Update данные.py

--- данные.py
+++ данные.py
@@ -3,28 +3,9 @@ import math as mh
 from scipy.optimize import newton
 C_0: float = 1.2
 C_1: float = 1.15
-t0 = 20 + 273
 
 
 class Parametrs:
-
-    def __init__(self, d, lambda_l, m_g, m_l, a_p, v_s, beta, f_ls, m_ls, theta, p_c, sigma_l, f_sc, delta, r_sw, p):
-        self.d = d
-        self.lambda_l = lambda_l
-        self.m_g = m_g
-        self.m_l = m_l
-        self.a_p = a_p
-        self.v_s = v_s
-        self.beta = beta
-        self.f_ls = f_ls
-        self.m_ls = m_ls
-        self.theta = theta
-        self.p_c = p_c
-        self.sigma_l = sigma_l
-        self.f_sc = f_sc
-        self.delta = delta
-        self.r_sw = r_sw
-        self.p = p
 
     @staticmethod
     def calc_rs(p: float, gamma_oil: float, gamma_gas: float, t: float) -> float:
@@ -45,8 +26,7 @@ class Parametrs:
         """
         yg = 1.225 + 0.00168 * t - 1.76875 / gamma_oil
         rs = gamma_gas * (1.924 * 10 ** (-6) * p / 10 ** yg) ** 1.205
-        print(t)
-        return rs
+        return float(rs)
 
     @staticmethod
     def calc_bo_st(rs: float, gamma_oil: float, gamma_gas: float, t: float) -> float:
@@ -67,7 +47,7 @@ class Parametrs:
         """
         bo = 0.972 + 0.000147 * (5.614583333333334 * rs * (gamma_gas / gamma_oil) ** 0.5 +
                                  2.25 * t - 574.5875) ** 1.175
-        return bo
+        return float(bo)
 
     @staticmethod
     def oil_bo_vb(p, compr, pb, bob):
@@ -86,20 +66,20 @@ class Parametrs:
         -------
         """
         oil_fvf_vasquez_above = bob * np.exp(compr * 145.03773773020924 * (pb - p))
-        return oil_fvf_vasquez_above
+        return float(oil_fvf_vasquez_above)
 
     @staticmethod
     def calc_bo(p, gamma_oil, gamma_gas, compr, pb, t):
         if p <= pb:
             rs = Parametrs.calc_rs(p, gamma_oil, gamma_gas, t)
             bo = Parametrs.calc_bo_st(rs, gamma_gas, gamma_oil, t)
-            return bo
+            return float(bo)
 
         rsb = Parametrs.calc_rs(pb, gamma_oil, gamma_gas, t)
         bob = Parametrs.calc_bo_st(rsb, gamma_gas, gamma_oil, t)
 
         bo = Parametrs.oil_bo_vb(p, compr, pb, bob)
-        return bo
+        return float(bo)
 
     @staticmethod
     def calc_oil_density(rs, bo, gamma_oil, gamma_gas):
@@ -121,7 +101,7 @@ class Parametrs:
         -------
         """
         oil_density = (1000 * gamma_oil + (rs * gamma_gas * 1.2217) / 1000) / bo
-        return oil_density
+        return float(oil_density)
 
     @staticmethod
     def pseudocritical_pressure(gamma_gas: float) -> float:
@@ -141,7 +121,7 @@ class Parametrs:
                 + 103421.3593975254 * gamma_gas
                 - 258553.39849381353 * (gamma_gas ** 2)
         )
-        return pc_p_standing
+        return float(pc_p_standing)
 
     @staticmethod
     def pseudocritical_temperature(gamma_gas: float) -> float:
@@ -161,11 +141,11 @@ class Parametrs:
                 + 180.55555555555554 * gamma_gas
                 - 6.944444444444445 * (gamma_gas ** 2)
         )
-        return pc_t_standing
+        return float(pc_t_standing)
 
     @staticmethod
     def dak_func(z, ppr, tpr):
-        ropr = 0.27 * (ppr / (z * tpr))
+        ropr = 0.27 * (ppr / (z * tpr))  # псевдоприведенная плотность
         func = (
                 -z
                 + 1
@@ -184,7 +164,8 @@ class Parametrs:
                 * (ropr ** 2 / tpr ** 3)
                 * np.exp(-0.7210 * ropr ** 2)
         )
-        return func
+
+        return float(func)
 
     @staticmethod
     def calc_gas_fvf(p: float, gamma_gas: float, t: float) -> float:
@@ -206,11 +187,11 @@ class Parametrs:
         tc = Parametrs.pseudocritical_temperature(gamma_gas)
 
         ppr = p / pc
-        tpr = t0 / tc
-        z = newton(Parametrs.dak_func, x0=1, args=(ppr, tpr), maxiter=300, rtol=0.2, tol=0.2)
+        tpr = t / tc
+        z = newton(Parametrs.dak_func, x0=1, args=(ppr, tpr), maxiter=3000, rtol=0.2, tol=0.2)
 
         bg = t * z * 350.958 / p
-        return bg
+        return float(bg)
 
     @staticmethod
     def calc_rho_gas(rs, bg, gamma_oil, gamma_gas):
@@ -223,7 +204,7 @@ class Parametrs:
         :return: плотность газа
         """
         rho_gas = (1000 * (gamma_oil + (rs * gamma_gas * 1.2217) / 1000)) / bg
-        return rho_gas
+        return float(rho_gas)
 
     # Вязкость
     @staticmethod
@@ -245,7 +226,7 @@ class Parametrs:
         a = 10.715 * (rs_new + 100) ** (-0.515)
         b = 5.44 * (rs_new + 150) ** (-0.338)
         oil_liveviscosity_beggs = a * oil_deadvisc ** b
-        return oil_liveviscosity_beggs
+        return float(oil_liveviscosity_beggs)
 
     @staticmethod
     def __oil_deadviscosity_beggs(gamma_oil, t):
@@ -264,81 +245,92 @@ class Parametrs:
         api = 141.5 / gamma_oil - 135.5
         x = 10 ** (3.0324 - 0.02023 * api) * t ** (-1.163)
         mu = 10 ** x - 1
-        return mu
+        return float(mu)
 
     @staticmethod
     def calc_viscosity(gamma_oil, gamma_gas, p, t):
         rs = Parametrs.calc_rs(p, gamma_oil, gamma_gas, t)
         mud = Parametrs.__oil_deadviscosity_beggs(gamma_oil, t)
         mus = Parametrs.__oil_liveviscosity_beggs(mud, rs)
-        return mus
+        return float(mus)
 
     @staticmethod
     def calc_debit_qo(q_lo, f_w, bo):
         q_oil = q_lo * (1 - f_w) * bo
-        return q_oil
+        return float(q_oil)
 
     @staticmethod
     def calc_debit_qw(q_lo, f_w, bw):
         q_water = q_lo * f_w * bw
-        return q_water
+        return float(q_water)
 
     @staticmethod
     def calc_debit_qg(q_oil, r_sb, rs, q_water, r_sw, bg):
         q_g = (q_oil * r_sb - q_oil * rs - q_water * r_sw) * bg
-        return q_g
+        return float(q_g)
 
     @staticmethod
     def calc_rho_l(oil_density, f_w, rho_w):
         rho_l = oil_density * (1 - f_w) + rho_w * f_w
-        return rho_l
+        return float(rho_l)
 
-    def pp_puz(self, rho_gas, rho_l):
-        p_tr = rho_l * self.lambda_l + rho_gas * (1 - self.lambda_l)
+    def pp_puz(self, rho_l, lambda_l, rho_gas):
+        p_tr = rho_l * lambda_l + rho_gas * (1 - lambda_l)
         return p_tr
 
-    def mp_puz(self):
-        m_tr = self.m_l * self.lambda_l + self.m_g * (1 - self.lambda_l)
+    def mp_puz(self, m_l, lambda_l, m_g):
+        m_tr = m_l * lambda_l + m_g * (1 - lambda_l)
         return m_tr
 
+    def np_puz(self, p_tr, v_tr, d, m_tr):
+        ne_tr = (p_tr * v_tr * d)/m_tr
+        return ne_tr
+
     """Пробковый режим"""
-    def v_tb(self, v_m, rho_l, rho_gas, q_g):
-        a_p = 200
+    def v_tb(self, v_m, rho_l, rho_gas, q_g, d, sigma_l):
+        a_p = mh.pi * 1600
         v_sg = q_g/a_p
-        v_tb = 1.2 * v_m + 0.35 * ((9.8 * self.d * (rho_l - rho_gas))/rho_l) ** 1/2
-        v_gls = 1.2 * v_m + 1.53 * ((9.8 * self.sigma_l * (rho_l - rho_gas))/rho_l) ** 1/4
+        v_tb = 1.2 * v_m + 0.35 * ((9.8 * d * (rho_l - rho_gas))/rho_l) ** 1/2
+        v_gls = 1.2 * v_m + 1.53 * ((9.8 * sigma_l * (rho_l - rho_gas))/rho_l) ** 1/4
         h_gls = v_sg / (0.425 + 2.65 * v_m)
         h_lls = 1 - h_gls
-        f_hltb = (9.916 * mh.sqrt(9.8 * self.d)) * (
+        f_hltb = (9.916 * mh.sqrt(9.8 * d)) * (
                 1 - mh.sqrt(1 - 0.15)) ** 0.5 * 0.15 - v_tb * (1 - 0.15) + h_gls * (v_tb - v_gls) + v_m
-        f_htb = v_tb + (9.916 * mh.sqrt(9.8 * self.d)) * (
+        f_htb = v_tb + (9.916 * mh.sqrt(9.8 * d)) * (
                 (1 - mh.sqrt(1 - 0.15)) ** 0.5 + 0.15 / (4 * mh.sqrt(1 - 0.15 * (1 - mh.sqrt(1 - 0.15)))))
         h_ltb = 0.15 - f_hltb/f_htb
         v_gtb = v_tb - ((v_tb - v_gls) * (1 - h_lls)) / (1 - h_ltb)
-        v_ltb = mh.sqrt(196.7 * 9.8 * self.sigma_l)
+        v_ltb = mh.sqrt(196.7 * 9.8 * sigma_l)
         v_lls = v_tb - (v_tb - (- v_ltb)) * h_ltb / h_lls
 
         return v_tb, v_gls, h_gls, h_lls, h_ltb, v_gtb, v_ltb, v_lls
 
     def p_pr(self, rho_l, h_lls, rho_gas):
+        # print(type(rho_l), type(h_lls), type(rho_gas))
         p_ls = rho_l * h_lls + rho_gas * (1 - h_lls)
         return p_ls
 
+    def ne_pr(self, p_ls, v_m, d, m_ls):
+        ne_ls = (p_ls * v_m * d)/m_ls
+        return ne_ls
+
     """Эмульсионный режим"""
-    def v_mus(self, v_sl):
-        v__sg = np.sin(self.theta * mh.pi/180) / (4 - 1.2) * 1.2 * v_sl + self.v_s
+    def v_mus(self, v_sl, theta, v_s):
+        v__sg = np.sin(theta * mh.pi/180) / (4 - 1.2) * 1.2 * v_sl + v_s
         return v__sg
 
+    #def ne_mus(self):
+
     """Кольцевой режим"""
-    def v_kol(self, rs, bg, gamma_oil, gamma_gas):
+    def v_kol(self, rs, bg, gamma_oil, gamma_gas, a_p):
         rho_gas = Parametrs.calc_rho_gas(rs, bg, gamma_oil, gamma_gas)
-        v_sg3 = rho_gas / self.a_p
+        v_sg3 = rho_gas / a_p
         return v_sg3
 
-    def vk_kol(self, v_sg3, rs, bg, gamma_oil, gamma_gas, oil_density, f_w, rho_w):
+    def vk_kol(self, v_sg3, rs, bg, gamma_oil, gamma_gas, oil_density, f_w, rho_w, m_g, sigma_l):
         rho_gas = Parametrs.calc_rho_gas(rs, bg, gamma_oil, gamma_gas)
         rho_l = Parametrs.calc_rho_l(oil_density, f_w, rho_w)
-        v_kr = 10000 * v_sg3 * self.m_g / self.sigma_l * (rho_gas / rho_l) ** 0.5
+        v_kr = 10000 * v_sg3 * m_g / sigma_l * (rho_gas / rho_l) ** 0.5
         return v_kr
 
     def f_kol(self, v_kr):
@@ -353,24 +345,40 @@ class Parametrs:
         v_sc = f_e * v_sl + v_sg3
         return v_sc
 
-    def df_kol(self, v_sc):
-        dp = self.f_sc * self.p_c * v_sc ** 2 / 2 * self.d
+    def df_kol(self, f_sc, p_c, v_sc, d):
+        dp = f_sc * p_c * v_sc ** 2 / 2 * d
         return dp
 
-    def z_kol(self, f_e, rho_l, rho_gas):
+    def z_kol(self, f_e, rho_l, rho_gas, delta):
         """
         коэффициент, связывающий силу трения с толщиной пленки
         """
         if f_e > 0.9:
-            z = 1 + 300 * self.delta
+            z = 1 + 300 * delta
         else:
-            z = 1 + 24 * self.delta * (rho_l / rho_gas) ** (1 / 3)
+            z = 1 + 24 * delta * (rho_l / rho_gas) ** (1 / 3)
         return z
 
-    def dp_c_kol(self, z, dp):
-        dp_c = z / (1 - 2 * self.delta) ** 5 * dp + self.p_c * 9.81 * np.sin(self.theta * mh.pi/180)
+    def dp_c_kol(self, z, dp, delta, p_c, theta):
+        dp_c = z / (1 - 2 * delta) ** 5 * dp + p_c * 9.81 * np.sin(theta * mh.pi/180)
         return dp_c
 
-    def fi_kol(self, dp_c, dp):
-        fi = dp_c - self.p_c * 9.81 * np.sin(self.theta * mh.pi/180) / dp
+    def l_lc(self, f_e, v_sl, v_sg):
+        lambda_lc = (f_e * v_sl)/(f_e * v_sl + v_sg)
+        return lambda_lc
+
+    def p_c_kol(self, rho_l, lambda_lc, rho_gas):
+        p_c = rho_l * lambda_lc + rho_gas * (1 - lambda_lc)
+        return p_c
+
+    def m_kol(self, m_l, lambda_lc, m_g):
+        m_sc = m_l * lambda_lc + m_g * (1 - lambda_lc)
+        return m_sc
+
+    def fi_kol(self, dp_c, p_c, theta, dp):
+        fi = dp_c - p_c * 9.81 * np.sin(theta * mh.pi/180) / dp
         return fi
+
+    def ne_kol(self, p_c, v_sc, d, m_sc):
+        ne_k = (p_c * v_sc * d)/m_sc
+        return ne_k
