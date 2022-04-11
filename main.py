02c5b11150fc5e4ed837de4f1cf8556867bb1579
Alina-Uteulina <a.uteulina@mail.ru>
2022-04-12 00:47:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,14 +7,13 @@ from scipy.optimize import fsolve
 
 
 class Ansari:
-    def __init__(self, d, lambda_l, m_g, a_p, v_s, beta, m_ls, sigma_l, delta, r_sw, f_w, rho_w, r_sb, gamma_oil,
+    def __init__(self, d, lambda_l, m_g, a_p, v_s, m_ls, sigma_l, delta, r_sw, f_w, rho_w, r_sb, gamma_oil,
                  gamma_gas, q_lo, q_go, bw, e):
         self.d = d
         self.lambda_l = lambda_l
         self.m_g = m_g
         self.a_p = a_p
         self.v_s = v_s
-        self.beta = beta
         self.m_ls = m_ls
         self.sigma_l = sigma_l
         self.delta = delta
@@ -43,18 +42,19 @@ class Ansari:
         """
         self.par = self.dan()
 
-        self.rs = Parametrs.calc_rs(p, self.gamma_oil, self.gamma_gas, t)
-        self.bo = Parametrs.calc_bo_st(self.rs, self.gamma_oil, self.gamma_gas, t)
-        self.bg = Parametrs.calc_gas_fvf(p, self.gamma_gas, t)
-        self.oil_density = Parametrs.calc_oil_density(self.rs, self.bo, self.gamma_oil, self.gamma_gas)
+        self.rs = self.par.calc_rs(p, self.gamma_oil, self.gamma_gas, t)
+        self.bo = self.par.calc_bo_st(self.rs, self.gamma_oil, self.gamma_gas, t)
+        self.bg = self.par.calc_gas_fvf(p, self.gamma_gas, t)
+        self.oil_density = self.par.calc_oil_density(self.rs, self.bo, self.gamma_oil, self.gamma_gas)
 
-        mus = Parametrs.calc_viscosity(self.gamma_oil, self.gamma_gas, p, t)
+        mus = self.par.calc_viscosity(self.gamma_oil, self.gamma_gas, p, t)
         self.m_l = mus * (1 - f_w) + m_w * f_w
+        self.m_tr = self.par.mp_puz(self.m_l, self.lambda_l, self.m_g)
 
-        self.rho_gas = Parametrs.calc_rho_gas(self.rs, self.bg, self.gamma_oil, self.gamma_gas)
-        self.rho_l = Parametrs.calc_rho_l(self.oil_density, self.f_w, self.rho_w)
+        self.rho_gas = self.par.calc_rho_gas(self.rs, self.bg, self.gamma_oil, self.gamma_gas)
+        self.rho_l = self.par.calc_rho_l(self.oil_density, self.f_w, self.rho_w)
 
-        self.q_oil = Parametrs.calc_debit_qo(self.q_lo, self.f_w, self.bo)
+        self.q_oil = self.par.calc_debit_qo(self.q_lo, self.f_w, self.bo)
         self.q_water = self.par.calc_debit_qw(self.q_lo, self.f_w, self.bw)
         self.q_g = self.par.calc_debit_qg(self.q_oil, self.r_sb, self.rs, self.q_water, self.r_sw, self.bg)
         q_l = self.q_oil + self.q_water
@@ -65,11 +65,13 @@ class Ansari:
         self.v_tr = self.v_m
 
         self.v_sg3 = self.par.v_kol(self.rs, self.bg, self.gamma_oil, self.gamma_gas, self.a_p)
-        v_kr = self.par.vk_kol(self.v_sg3, self.rs, self.bg, self.gamma_oil, self.gamma_gas, self.oil_density, self.f_w,
-                               self.rho_w, self.m_g, self.sigma_l)
+        v_kr = self.par.vk_kol(self.v_sg3, self.m_g, self.sigma_l, self.rho_l, self.rho_gas)
         self.f_e = self.par.f_kol(v_kr)
         lambda_lc = self.par.l_lc(self.f_e, self.v_sl, self.v_sg)
         self.p_c = self.par.p_c_kol(self.rho_l, lambda_lc, self.rho_gas)
+        self.beta = self.par.v_tb(self.v_m, self.rho_l, self.rho_gas, self.q_g, self.d, self.sigma_l)[8]
+        self.p_tr = self.par.pp_puz(self.rho_l, self.lambda_l, self.rho_gas)
+        self.n_e = self.par.n_e(self.p_tr, self.v_tr, self.d, self.m_tr)
 
     def calc_fp(self):
 
@@ -86,8 +88,6 @@ class Ansari:
         sg1 = 0.25 * self.v_s + 0.333 * self.v_sl
         sg4 = 3.1 * (9.81 * self.sigma_l * (self.rho_l - self.rho_gas) / self.rho_gas ** 2) ** 1 / 4
 
-        v__sg = self.par.v_mus(self.v_sl, self.theta, self.v_s)
-
         self.v_sc = self.par.vs_kol(self.f_e, self.v_sl, self.v_sg3)
 
         if self.v_sl < sg1:
@@ -96,77 +96,65 @@ class Ansari:
         elif self.v_m > sg1:
             fp = 2
             return fp
-        elif v__sg < sg4:
-            fp = 3
-            return fp
         elif self.v_sc > sg4:
             fp = 4
             return fp
         else:
             return 1
 
-    def calc_ftr(self, n_e):
+    def calc_f(self, f, n_e):
+        func = 1.74 * 2 * math.log((2 * self.e)/d + 18.7/(n_e * math.sqrt(f))) - 1/math.sqrt(f)
+        return func
+
+    def calc_ftr(self):
         """
         Рассчет силы трения
-        :return:
         """
-        self.l1 = 64 / n_e  # ламинарный режим
-        self.l2 = fsolve(1.74 * 2 * math.log((2 * self.e)/d + 18.7/(ne * math.sqrt(f))))  # турбулентный режим
 
-        m_tr = self.par.mp_puz(self.m_l, lambda_l, self.m_g)
-        self.p_tr = self.par.pp_puz(self.rho_l, self.lambda_l, self.rho_gas)
-        ne_tr = self.par.np_puz(self.p_tr, self.v_tr, self.d, m_tr)
+        ne_tr = self.par.n_e(self.p_tr, self.v_tr, self.d, self.m_tr)
 
         if ne_tr < 2000:
-            ne_tr = n_e
-            f_tr = self.l1
-            return f_tr
+            f_tr = 64 / ne_tr  # ламинарный режим
         else:
-            ne_tr = ne
-            f_tr = self.l2
-            return f_tr
+            f_tr = fsolve(self.calc_f, args=(ne_tr), x0=1)  # турбулентный режим
+        return f_tr
 
     def calc_fls(self):
 
         h_lls = self.par.v_tb(self.v_m, self.rho_l, self.rho_gas, self.q_g, self.d, self.sigma_l)[3]
         self.p_ls = self.par.p_pr(self.rho_l, h_lls, self.rho_gas)
-        ne_ls = self.par.ne_pr(self.p_ls, self.v_m, self.d, m_ls)
+        ne_ls = self.par.n_e(self.p_ls, self.v_m, self.d, self.m_ls)
 
         if ne_ls < 2000:
-            ne_ls = n_e
-            f_ls = self.l1
-            return f_ls
+            f_ls = 64 / ne_ls  # ламинарный режим
         else:
-            ne_ls = ne
-            f_ls = self.l2
-            return f_ls
+            f_ls = fsolve(self.calc_f, args=(ne_ls), x0=1)  # турбулентный режим
+        return f_ls
 
     def calc_f_sc(self):
 
-        m_sc = self.par.m_kol(self.m_l, lambda_lc, self.m_g))
-        ne_k = self.par.ne_kol(self.p_c, self.v_sc, self.d, m_sc)
+        lambda_lc = self.par.l_lc(self.f_e, self.v_sl, self.v_sg)
+        m_sc = self.par.m_kol(self.m_l, lambda_lc, self.m_g)
+        ne_k = self.par.n_e(self.p_c, self.v_sc, self.d, m_sc)
 
         if ne_k < 2000:
-            ne_k = n_e
-            f_sc = self.l1
-            return f_sc
+            f_sc = 64 / ne_k
         else:
-            ne_k = ne
-            f_sc = self.l2
-            return f_sc
+            f_sc = fsolve(self.calc_f, args=(ne_k), x0=1)  # турбулентный режим
+        return f_sc
 
-    def puz(self, p_tr, f_tr, v_tr):
+    def puz(self, f_tr):
         """
         Функция расчета градиента для пузырькового режима
 
         """
 
-        dp_dl_grav = (p_tr * 9.81 * np.sin(self.theta * math.pi / 180))  # гравитационная составляющая
-        dp_dl_fric = (f_tr * p_tr * v_tr ** 2 / 2 * self.d)  # составляющая по трению
+        dp_dl_grav = (self.p_tr * 9.81 * np.sin(self.theta * math.pi / 180))  # гравитационная составляющая
+        dp_dl_fric = (f_tr * self.p_tr * self.v_tr ** 2) / (2 * self.d)  # составляющая по трению
 
-        return -(dp_dl_grav + dp_dl_fric)
+        return dp_dl_grav + dp_dl_fric
 
-    def prob(self, f_ls, v_m):
+    def prob(self, f_ls):
         """
         расчет градиента давления для пробкового режима
 
@@ -174,41 +162,26 @@ class Ansari:
 
         dp_dl_grav = ((1 - self.beta) * self.p_ls + self.beta * self.rho_gas) * 9.81 * np.sin(
             self.theta * math.pi / 180)  # гравитационная составляющая
-        dp_dl_fric = f_ls * self.p_ls * v_m ** 2 / 2 * self.d * (1 - self.beta)  # составляющая по трению
+        dp_dl_fric = f_ls * self.p_ls * self.v_m ** 2 / (2 * self.d) * (1 - self.beta)  # составляющая по трению
 
-        return -(dp_dl_grav + dp_dl_fric)
+        return dp_dl_grav + dp_dl_fric
 
-    def muz(self, f_tr, p_tr, v_tr):
-        """
-        расчет градиенты давления для эмульсионного режима
-
-        Parameters
-        ----------
-        :param f_tr: сила трения
-        :param v_tr: скорость двухфазного потока
-        """
-        dp_dl_grav = (f_tr * p_tr * v_tr ** 2 / 2 * self.d)  # гравитационная составляющая
-        dp_dl_fric = p_tr * 9.81 * np.sin(self.theta * math.pi / 180)  # составляющая по трению
-
-        return -(dp_dl_grav + dp_dl_fric)
-
-    def kol(self, fi, dp):
+    def kol(self, f_sc):
         """
         расчет давления для кольцевого режима
 
         Parameters
         ----------
-        :param p_c: плотность газового ядра
         """
 
         self.dp = self.par.df_kol(f_sc, self.p_c, self.v_sc, self.d)
         z = self.par.z_kol(self.f_e, self.rho_l, self.rho_gas, self.delta)
-        dp_c = self.par.dp_c_kol(z, dp, self.delta, self.p_c, self.theta)
-        self.fi = self.par.fi_kol(dp_c, self.p_c, self.theta, self.dp)
+        dp_c = self.par.dp_c_kol(z, self.dp, self.delta, self.p_c, self.theta)
+        fi = self.par.fi_kol(dp_c, self.p_c, self.theta, self.dp)
 
-        dp_dl_grav = fi * dp  # гравитационная составляющая
+        dp_dl_grav = fi * self.dp  # гравитационная составляющая
         dp_dl_fric = self.p_c * 9.81 * np.sin(self.theta * math.pi / 180)  # составляющая по трению
-        return -(dp_dl_grav + dp_dl_fric)
+        return dp_dl_grav + dp_dl_fric
 
     def grad(self, h, pt):
 
@@ -218,22 +191,25 @@ class Ansari:
         fp = self.calc_fp()
 
         if fp == 1:
-            gr = self.puz(self.p_tr, f_tr, self.v_tr)
+            f_tr = self.calc_ftr()
+            gr = self.puz(f_tr)
         elif fp == 2:
-            gr = self.prob(f_ls, self.v_m)
-        elif fp == 3:
-            gr = self.muz(f_tr, self.p_tr, self.v_tr)
+            f_ls = self.calc_fls()
+            gr = self.prob(f_ls)
         elif fp == 4:
-            gr = self.kol(self.fi, self.dp)
+            f_sc = self.calc_f_sc()
+            gr = self.kol(f_sc)
 
         return gr, self.dt
 
     def calc_crd(self, h, p_wh, t_wh):
-        #cl = anouther_class()
+        rs, bo, bg, oil_density, m_l, m_tr, rho_gas, rho_l, q_oil, q_water, q_g, v_sl, v_sg, v_m, v_tr, v_sg3, f_e, p_c, beta, p_tr, n_e = self.calc_params(p_wh, t_wh)
         result = solve_ivp(
             self.grad,
             t_span=[0, h],
-            y0=[p_wh * 101325, t_wh + 273]
+            y0=[p_wh * 101325, t_wh + 273],
+            args=(rs, bo, bg, oil_density, m_l, m_tr, rho_gas, rho_l, q_oil, q_water, q_g, v_sl, v_sg, v_m, v_tr, v_sg3,
+                  f_e, p_c, beta, p_tr, n_e, dp, v_sc, p_ls)
         )
         return result.t, result.y[0, :]
 
@@ -245,8 +221,8 @@ r_sb = 200
 r_sw = 0.2
 f_w = 1  # обводненность
 bw = 1
-q_lo = 100
-q_go = 100
+q_lo = 100/86400
+q_go = 100/86400
 sigma_l = 1.5
 gamma_gas = 0.7  # относительная плотность газа, Па
 gamma_oil = 0.801  # относительная плотность нефти, Па
@@ -256,8 +232,6 @@ compr = 9.87 * 10 ** (-10)
 d = 0.08
 a_p = math.pi * 0.0016
 v_s = 50
-beta = 10
-fi = 100
 p = 101325
 lambda_l = 1
 m_g = 1
@@ -271,7 +245,7 @@ p_wh = 10  # давление на устье, атм
 t_wh = 20  # температура на устье, С
 
 # Запуск расчета
-ans = Ansari(d, lambda_l, m_g, a_p, v_s, beta, m_ls, sigma_l, delta, r_sw, f_w, rho_w, r_sb, gamma_oil, gamma_gas, q_lo,
+ans = Ansari(d, lambda_l, m_g, a_p, v_s, m_ls, sigma_l, delta, r_sw, f_w, rho_w, r_sb, gamma_oil, gamma_gas, q_lo,
              q_go, bw, e)
 crd = ans.calc_crd(h, p_wh, t_wh)
 
